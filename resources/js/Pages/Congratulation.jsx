af83ccccbbc54b00f9425a719d58fc228d408aaf
Ceejay Santos <7078ceejay@gmail.com>
2025-01-27 02:36:09 +0800

    w/ star wars credit page

--- resources/js/Pages/Congratulation.jsx
+++ resources/js/Pages/Congratulation.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 function Congratulation() {
   return (
-    <div>Congratulation</div>
+    <div className="credit-container">
+    <div className="crawl">
+      <h1 className="crawl-title">Credits</h1>
+      <div className="crawl-content">
+        <p>Directed by: John Doe</p>
+        <p>Produced by: Jane Smith</p>
+        <p>Starring:</p>
+        <p>Actor One</p>
+        <p>Actor Two</p>
+        <p>Actor Three</p>
+        <p>Music by: Composer Name</p>
+        <p>Visual Effects by: VFX Studio</p>
+        <p>And many more...</p>
+        <p>May the Force be with you!</p>
+      </div>
+    </div>
+    <div className="stars"></div>
+  </div>
   )
 }
 
